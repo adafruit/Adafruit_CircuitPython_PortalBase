e32917ad0a77ab9ed8a9c258dcfbfe7169bfb55d
dherrada <dylan.herrada@adafruit.com>
2021-11-05 15:28:23 -0400

    Removed unnecessary pylint disable

--- adafruit_portalbase/__init__.py
+++ adafruit_portalbase/__init__.py
@@ -400,9 +400,7 @@ class PortalBase:
                     string = func(values[value_index])
                 else:
                     try:
-                        string = "{:,d}".format(
-                            int(values[value_index])
-                        )  # pylint: disable=consider-using-f-string
+                        string = "{:,d}".format(int(values[value_index]))
                     except (TypeError, ValueError):
                         string = values[value_index]  # ok it's a string
                 self._fetch_set_text(string, index=i)
