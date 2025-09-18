38f57004368a54b63c382895fae6fa52c3e29148
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2025-09-18 13:49:39 -0500

    Switch from `type` to `isinstance`

--- adafruit_portalbase/network.py
+++ adafruit_portalbase/network.py
@@ -391,8 +391,8 @@ class NetworkBase:
                     lambda credentials: isinstance(credentials, (list, tuple))
                     and "ssid" in credentials
                     and "password" in credentials
-                    and type(credentials["ssid"]) is str
-                    and type(credentials["password"]) is str
+                    and isinstance(credentials["ssid"], str)
+                    and isinstance(credentials["password"], str)
                     and len(credentials["ssid"])
                     and len(credentials["password"]),
                     self._wifi_credentials,
