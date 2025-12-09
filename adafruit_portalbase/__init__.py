dc7ecff7794435053cc4ab2076978a9b35149009
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2025-12-09 10:50:41 -0600

    Fix `OutlinedLabel` deprecation error

--- adafruit_portalbase/__init__.py
+++ adafruit_portalbase/__init__.py
@@ -28,7 +28,6 @@ import terminalio
 from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text import wrap_text_to_lines
 from adafruit_display_text.bitmap_label import Label
-from adafruit_display_text.outlined_label import OutlinedLabel
 
 __version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_PortalBase.git"
@@ -291,7 +290,7 @@ class PortalBase:
                         scale=self._text[index]["scale"],
                     )
                 else:
-                    self._text[index]["label"] = OutlinedLabel(
+                    self._text[index]["label"] = Label(
                         self._fonts[self._text[index]["font"]],
                         text=string,
                         scale=self._text[index]["scale"],
