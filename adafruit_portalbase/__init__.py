ddaaa10bafbfa083ba4ee23bdebbf5ce60f00348
Melissa LeBlanc-Williams <melissa@adafruit.com>
2025-12-10 12:03:08 -0800

    Fix OutlinedLabel import, but remain backwards compatible

--- adafruit_portalbase/__init__.py
+++ adafruit_portalbase/__init__.py
@@ -28,7 +28,11 @@ import terminalio
 from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text import wrap_text_to_lines
 from adafruit_display_text.bitmap_label import Label
-from adafruit_display_text.outlined_label import OutlinedLabel
+
+if not hasattr(Label, "outline_size"):
+    from adafruit_display_text.outlined_label import OutlinedLabel
+else:
+    OutlinedLabel = Label
 
 __version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_PortalBase.git"
